f5574980755d92b50ce468de0fec477d2292b661
Vladimir Gerasimenko <vol.gerasimenko@gmail.com>
2018-09-04 13:15:48 +0300

    Add page home

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -24,6 +24,11 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'indent': 'off',
+    'vue/script-indent': ['error', 2, {
+      'baseIndent': 1,
+      "switchCase": 1
+    }]
   }
 }
--- client/src/pages/home/index.vue
+++ client/src/pages/home/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div>
+    homepage
   </div>
 </template>
 
 <script>
   export default {
-    name: 'App'
+    name: 'home'
   }
 </script>
+
+<style scoped>
+
+</style>
